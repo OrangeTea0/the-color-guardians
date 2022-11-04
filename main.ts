56c4d53924e1c6d7405029a18cc35621b2e6cdab
OrangeTea0 <102241514+OrangeTea0@users.noreply.github.com>
2022-11-04 15:20:20 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    music.playTone(294, music.beat(BeatFraction.Eighth))
+    music.playTone(294, music.beat(BeatFraction.Eighth))
+    music.playTone(294, music.beat(BeatFraction.Eighth))
+})
+basic.forever(function () {
+	
+})
